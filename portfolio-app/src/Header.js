87478b3090f6937a83eb6b905f1eefaa1dfa994b
madpgmer <madpgmer@gmail.com>
2022-04-13 10:24:04 -0600

    Personalized Header with links and image
    Added email, linkedin and github links
    Added image

--- portfolio-app/src/Header.js
+++ portfolio-app/src/Header.js
@@ -1,25 +1,30 @@
+import image from './Madhu.png';
+
+console.log(image);
+
 function Header(){
     return (
         <header className="text-white bg-dark">
         <div className="container py-5">
             <div className="row">
                 <div className="col-xs-3 col-lg-2 text-center">
-                    <img src="https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"
-                        alt="Person - Avatar Placeholder@seekpng.com" className="border border-5 rounded-circle img-fluid" style={{width: '150px', height: '150px'}}/>
+                    <img src={image}
+                        alt="selfimage" style={{width: '150px', height: '150px'}}
+                        />
                 </div>
                 <div className="col text-center text-lg-start">
-                    <h1>Joe Blow</h1>
+                    <h1>Madhu Madhavan</h1>
 
                     <p className="lead">Software development student at SAIT (Southern Alberta Institute of Technology).</p>
 
                     <ul className="list-inline">
-                        <li className="list-inline-item my-2"><a href="joeblow@gmail.com" className="fs-5 link-light text-decoration-none"><i
+                        <li className="list-inline-item my-2"><a href="madpgmer@gmail.com" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-envelope pe-1"></i>
                                 <span className="text-center">E-mail</span></a></li>
-                        <li className="list-inline-item my-2"><a href="www.linkedin.com" className="fs-5 link-light text-decoration-none"><i
+                        <li className="list-inline-item my-2"><a href="https://www.linkedin.com/in/madhu-madhavan2022" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-linkedin pe-1"></i>
                                 LinkedIn</a></li>
-                        <li className="list-inline-item my-2"><a href="www.github.com" className="fs-5 link-light text-decoration-none"><i
+                        <li className="list-inline-item my-2"><a href="https://github.com/madpgmer" className="fs-5 link-light text-decoration-none"><i
                                     className="bi bi-github pe-1"></i>
                                 GitHub</a></li>
                     </ul>
