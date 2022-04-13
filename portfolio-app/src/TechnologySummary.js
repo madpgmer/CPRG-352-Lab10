eb7afce1df03f4363e1e58c9bb1c40ded0dcf562
madpgmer <madpgmer@gmail.com>
2022-04-13 11:03:25 -0600

    Personalized Tech Summary

--- portfolio-app/src/TechnologySummary.js
+++ portfolio-app/src/TechnologySummary.js
@@ -5,37 +5,78 @@ function TechnologySummary() {
                         <h2 className="card-title">Technology Summary</h2>
                         <ul className="list-unstyled">
                             <li>
-                                <h6 className="fw-bold">Senior Web Developer / Digital Agency 2016-2020</h6>
-                                <p>Phasellus et tellus iaculis, interdum augue vel, luctus nulla. Aenean viverra,
-                                    magna
-                                    a
-                                    ultricies
-                                    elementum, dui mi tristique ligula, non euismod leo mauris ac metus.</p>
-                            </li>
-                            <li>
-                                <h6 className="fw-bold">Web Developer / Big Firm 2010-2015</h6>
-                                <p>Mauris volutpat, ex condimentum fringilla imperdiet, nulla turpis semper diam, ac
-                                    semper
-                                    risus urna quis
-                                    libero. Fusce non nunc quam. Ut vulputate, magna eget molestie luctus, elit est
-                                    posuere
-                                    lorem, quis
-                                    dapibus dolor felis id mi.</p>
-                            </li>
-                            <li>
-                                <h6 className="fw-bold">Junior Web Developer / Websites 'R' Us 2004-2009</h6>
-                                <p>Sed eu turpis placerat, posuere odio a, viverra velit. Pellentesque habitant
-                                    morbi
-                                    tristique
-                                    senectus et
-                                    netus et malesuada fames ac turpis egestas.</p>
+                                <table class="table table-bordered table-dark">
+                                    <thead>
+                                        <tr>
+                                        <th scope="row">Web Development</th>
+                                        <td>Java, HTML5, XML, JavaScript, Nodejs,JSP, Java Servlet, Java Beans, CSS, Bootstrap, AJAX, REACT</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Networking</th>
+                                        <td>DHCP, DNS, TCP/IP, Cisco Packet Tracer, Putty SSH & Telnet Client</td>
+                                        </tr>  
+                                        
+                                        <tr>	
+                                        <th scope="row">ERP and CRM</th>
+                                        <td>Salesforce, Goldmine, PeopleSoft - HRMS, EPM, SCM, POS, Inventory Control Management</td>
+                                        </tr>  
+                                        
+                                        <tr>	
+                                        <th scope="row">Microsoft and other technologies</th>
+                                        <td>MS Office suite, MS Project, MS SQL Server, MS teams, Visual Studio Code, MS SharePoint, Microsoft Visio, Software Ideas Modeler, MIRA</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Graphics packages </th>
+                                        <td>Adobe Photoshop, Adobe Lightroom, Adobe XD, Justinmind, Figma</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">IDE</th>
+                                        <td>Eclipse, Blue Jay, NetBeans, IntelliJ</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Webserver</th>
+                                        <td>Tomcat 9, Amazon EC2</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Database</th>
+                                        <td>Oracle 12, PL/SQL, HeidiSQL, MySQL, MongoDB</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Testing Tools</th>
+                                        <td>JIRA, QA Complete Selenium Web Driver, HP UFT-QTP, LoadRunner, Performance Center</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Automation Server and tools</th>
+                                        <td>Jenkins, Maven, JUnit-on Eclipse</td>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th scope="row">Version Control</th>
+                                        <td>GitHub, Subversion</td>	  
+                                        </tr>
+                                    </thead>
+                                </table>
+                                <h6 className="fw-bold">Key Skills</h6>
+                                <p>•	Strong background working with clients and in-house developers to identify requirements, assist with creating user interfaces, and develop implementation timelines</p>
+                                <p>•	In-depth understanding of project lifecycle, including all phases of system and software development</p>
+                                <p>•	Performed data analysis, testing, and debugging </p>
+                                <p>•	Proficient in Object-Oriented methodologies. Leverage knowledge of object-oriented programming to help validate, verify, communicate, and resolve software issues through careful, thoroughly documented testing to maximize quality of software</p>
+                                <p>•	Developed Process flow, Activity, Use Case, Class and Sequence Diagrams </p>
+                                <p>•	Possess excellent customer service skills with ability to deal with every kind of customer personality</p>
+                                <p>•	A team player who is highly energetic, detail oriented, self-managed, driven, and competitive</p>
+                                <p>•	Excellent written and verbal communication, interpersonal, liaison and presentation skills</p>
+                                <p>•	Capable of facilitating workshops for core team and employee benefit programs</p>
                             </li>
                         </ul>
                     </div>
                 </div>
-            
-       
-
     );
 }
 export default TechnologySummary;
